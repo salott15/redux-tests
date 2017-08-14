195d8db71765a23e59d091f46072326f73f3c162
salott15 <sarilott@yahoo.com>
2017-08-14 13:43:50 -0700

    new commit

---
+++ src/components/top-nav.test.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+
+import {TopNav} from './top-nav';
+import {NEW_GAME, toggleInfoModal} from '../actions';
+
+describe('<TopNav />', () => {
+	it('Renders without crashing', () => {
+		shallow(<TopNav />);
+	});
+
+	it('Should dispatch newGame when new game is clicked', () => {
+		const dispatch = jest.fn();
+		const wrapper = shallow(<TopNav dispatch={dispatch} />);
+		const link = wrapper.find('new');
+		link.simulate('click', {
+			preventDefault() {}
+		});
+		expect(dispatch).toHaveBeenCalled();
+		expect(dispatch.mock.calls[0][0].type).toEqual(NEW_GAME);
+	});
+
+    it('Should dispatch toggleInfoModal when info is clicked', () => {
+        const dispatch = jest.fn();
+        const wrapper = shallow(<TopNav dispatch={dispatch} />);
+        const link = wrapper.find('.what');
+        link.simulate('click', {
+            preventDefault() {}
+        });
+        expect(dispatch).toHaveBeenCalled();
+        expect(dispatch.mock.calls[0][0]).toEqual(toggleInfoModal());
+    });
+});
